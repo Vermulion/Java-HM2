d53b12cafa2352be608fd0edd7fc1a3e512d9405
Viacheslav Ryabov <vermulion@gmail.com>
2022-07-05 00:25:21 +0300

    2nd task was made, 1st one was corrected

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         int ticket = 1099; //Водим цену билета
         int miles = ticket / 20;
-        String name = " Bonus miles";
-        System.out.println("You have:");
+        String name = " Бонусных миль";
+        System.out.println("На вашем счете:");
         System.out.println(miles + name);
     }
 }
