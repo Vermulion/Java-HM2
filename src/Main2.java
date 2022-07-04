2856aa58f936d63ef346d303855ac517c9378e89
Viacheslav Ryabov <vermulion@gmail.com>
2022-07-05 00:23:15 +0300

    2nd task was made

--- src/Main2.java
+++ src/Main2.java
@@ -1,7 +1,20 @@
 // Задание 2
 public class Main2 {
     public static void main(String[] args) {
-    int account = 500 // начальный аккаунт
+        int account = 656; // начальный аккаунт
+        int cash = 1001; // сумма пополнения
+        int bonus; // начисляемые бонусные рубли
 
+        if (cash > 1000) {
+            bonus = (int) (0.01 * cash);
+        } else {
+            bonus = 0;
+        }
+        int final_account = account + cash + bonus; // итоговый счет после пополнения
+        String rub = " руб.";
+        String acc = "Баланс счета: ";
+        String gratification = "Начисленные бонусные рубли: ";
+        System.out.println(acc + final_account + rub);
+        System.out.println(gratification + bonus + rub);
     }
 }
